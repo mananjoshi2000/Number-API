e9a94f96ec97d3bfead815b3c61a87444caae0ab
Manan Joshi <mananj0806@gmail.com>
2020-04-21 03:55:22 +0530

    first commit
    Signed-off-by: Manan Joshi <mananj0806@gmail.com>

---
+++ app.js
@@ -0,0 +1,56 @@
+const math = document.getElementById('mathematics');
+const math_btn = document.getElementById('math-btn');
+const math_input = document.getElementById('math-input');
+const text_math = document.querySelector('.text-math');
+
+math_btn.addEventListener('click',()=>{
+    
+    fetch(`http://numbersapi.com/${math_input.value}/math`)
+    .then((res)=> {
+        return res.text();
+    })
+    .then((txt)=>{
+        math.style.backgroundImage = 'linear-gradient(rgba( 0, 0, 0, 0.8),rgba( 0, 0, 0, 0.8)),url("math2.jpg")';
+        text_math.innerHTML = `<p> ${txt} </p>`
+    })
+    .catch((err)=>alert(err))
+});
+
+const trivia = document.getElementById('trivia');
+const trivia_btn = document.getElementById('trivia-btn');
+const trivia_input = document.getElementById('trivia-input');
+const text_trivia = document.querySelector('.text-trivia');
+
+trivia_btn.addEventListener('click',()=>{
+    
+    fetch(`http://numbersapi.com/${trivia_input.value}/trivia`)
+    .then((res)=> {
+        return res.text();
+    })
+    .then((txt)=>{
+        console.log(txt);
+        trivia.style.backgroundImage = 'linear-gradient(rgba( 0, 0, 0, 0.8),rgba( 0, 0, 0, 0.8)),url("trivia5.jpg")';
+        text_trivia.innerHTML = `<p> ${txt} </p>`
+    })
+    .catch((err)=>alert(err))
+});
+
+
+const year = document.getElementById('year');
+const year_btn = document.getElementById('year-btn');
+const year_input = document.getElementById('year-input');
+const text_year = document.querySelector('.text-year');
+
+year_btn.addEventListener('click',()=>{
+    
+    fetch(`http://numbersapi.com/${year_input.value}/year`)
+    .then((res)=> {
+        return res.text();
+    })
+    .then((txt)=>{
+        year.style.backgroundImage = 'linear-gradient(rgba( 0, 0, 0, 0.8),rgba( 0, 0, 0, 0.8)),url("year.jpg")';
+        text_year.innerHTML = `<p> ${txt} </p>`
+    })
+    .catch((err)=>alert(err))
+});
+
